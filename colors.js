0e03700e2ed8866312a0f47e3a7383d223beaf77
tonyd238 <42108517+tonyd238@users.noreply.github.com>
2018-11-01 20:27:12 +0200

    adfasdasd

---
+++ colors.js
@@ -0,0 +1,278 @@
+$(document).ready(function(){
+
+		$("#pink").click(function(){
+
+			$("#image").attr({"src":"bp-colors/mini/pink.jpg"});
+			$("#colorname").text("Pink");
+		});
+		$("#light-blue").click(function(){
+
+			$("#image").attr({"src":"bp-colors/mini/blue.jpg"});
+			$("#colorname").text("UN Blue");
+		});
+		$("#blue-red").click(function(){
+
+			$("#image").attr({"src":"bp-colors/mini/blue-red.jpg"});
+			$("#colorname").text("Royal Blue/Ox Red");
+		});
+		$("#sky-blue").click(function(){
+
+			$("#image").attr({"src":"bp-colors/mini/light-blue.jpg"});
+			$("#colorname").text("Sky Blue");
+		});
+		$("#pink-green").click(function(){
+
+			$("#image").attr({"src":"bp-colors/mini/pink-blue.jpg"});
+			$("#colorname").text("Frost Green/Peach Pink");
+		});
+		$("#graphite").click(function(){
+
+			$("#image").attr({"src":"bp-colors/mini/graphite.jpg"});
+			$("#colorname").text("Graphite");
+		});
+		$("#rose-pink").click(function(){
+
+			$("#image").attr({"src":"bp-colors/mini/rose-blue.jpg"});
+			$("#colorname").text("Peach Pink");
+		});
+		$("#red").click(function(){
+
+			$("#image").attr({"src":"bp-colors/mini/red.jpg"});
+			$("#colorname").text("Ox Red");
+		});
+		$("#royal-blue").click(function(){
+
+			$("#image").attr({"src":"bp-colors/mini/royal-blue.jpg"});
+			$("#colorname").text("Royal Blue");
+		});
+		$("#orange").click(function(){
+
+			$("#image").attr({"src":"bp-colors/mini/orange.jpg"});
+			$("#colorname").text("Brick");
+		});
+		$("#black").click(function(){
+
+			$("#image").attr({"src":"bp-colors/mini/black.jpg"});
+			$("#colorname").text("Black");
+		});
+		$("#red-black").click(function(){
+
+			$("#image").attr({"src":"bp-colors/mini/red-black.jpg"});
+			$("#colorname").text("Black/Ox Red");
+		});
+		$("#yellow-blue").click(function(){
+
+			$("#image").attr({"src":"bp-colors/mini/yellow-blue.jpg"});
+			$("#colorname").text("Navy/Warm Yellow");
+		});
+		$("#red-blue").click(function(){
+
+			$("#image").attr({"src":"bp-colors/mini/red-blue.jpg"});
+			$("#colorname").text("Ox Red/Royal Blue");
+		});
+		$("#violet").click(function(){
+
+			$("#image").attr({"src":"bp-colors/mini/violet.jpg"});
+			$("#colorname").text("Purple");
+		});
+		$("#green").click(function(){
+
+			$("#image").attr({"src":"bp-colors/mini/green.jpg"});
+			$("#colorname").text("Forest Green");
+		});
+		$("#light-green").click(function(){
+
+			$("#image").attr({"src":"bp-colors/mini/light-green.jpg"});
+			$("#colorname").text("Frost Green");
+		});
+		$("#yellow").click(function(){
+
+			$("#image").attr({"src":"bp-colors/mini/yellow.jpg"});
+			$("#colorname").text("Warm Yellow");
+		});
+		$("#grey").click(function(){
+
+			$("#image").attr({"src":"bp-colors/mini/gret.jpg"});
+			$("#colorname").text("Fog");
+		});
+			/*for classic backpacks*/
+
+				$("#pink-c").click(function(){
+
+			$("#image-c").attr({"src":"bp-colors/classic/pink.jpg"});
+			$("#colorname").text("Pink");
+		});
+		$("#light-blue-c").click(function(){
+
+			$("#image-c").attr({"src":"bp-colors/classic/light-blue.jpg"});
+			$("#colorname").text("UN Blue");
+		});
+		$("#blue-red-c").click(function(){
+
+			$("#image-c").attr({"src":"bp-colors/classic/blue-red.jpg"});
+			$("#colorname").text("Royal Blue/Ox Red");
+		});
+		$("#sky-blue-c").click(function(){
+
+			$("#image-c").attr({"src":"bp-colors/classic/light-blue.jpg"});
+			$("#colorname").text("Sky Blue");
+		});
+		$("#pink-green-c").click(function(){
+
+			$("#image-c").attr({"src":"bp-colors/classic/pink-green.jpg"});
+			$("#colorname").text("Frost Green/Peach Pink");
+		});
+		$("#graphite-c").click(function(){
+
+			$("#image-c").attr({"src":"bp-colors/classic/graphite.jpg"});
+			$("#colorname").text("Graphite");
+		});
+		$("#rose-pink-c").click(function(){
+
+			$("#image-c").attr({"src":"bp-colors/classic/rose-pink.jpg"});
+			$("#colorname").text("Peach Pink");
+		});
+		$("#red-c").click(function(){
+
+			$("#image-c").attr({"src":"bp-colors/classic/red.jpg"});
+			$("#colorname").text("Ox Red");
+		});
+		$("#royal-blue-c").click(function(){
+
+			$("#image-c").attr({"src":"bp-colors/classic/royal-blue.jpg"});
+			$("#colorname").text("Royal Blue");
+		});
+		$("#orange-c").click(function(){
+
+			$("#image-c").attr({"src":"bp-colors/classic/orange.jpg"});
+			$("#colorname").text("Brick");
+		});
+		$("#black-c").click(function(){
+
+			$("#image-c").attr({"src":"bp-colors/classic/black.jpg"});
+			$("#colorname").text("Black");
+		});
+		$("#red-black-c").click(function(){
+
+			$("#image-c").attr({"src":"bp-colors/classic/red-black.jpg"});
+			$("#colorname").text("Black/Ox Red");
+		});
+		$("#yellow-blue-c").click(function(){
+
+			$("#image-c").attr({"src":"bp-colors/classic/yellow-blue.jpg"});
+			$("#colorname").text("Navy/Warm Yellow");
+		});
+		$("#red-blue-c").click(function(){
+
+			$("#image-c").attr({"src":"bp-colors/classic/red-blue.jpg"});
+			$("#colorname").text("Ox Red/Royal Blue");
+		});
+		$("#violet-c").click(function(){
+
+			$("#image-c").attr({"src":"bp-colors/classic/violet.jpg"});
+			$("#colorname").text("Purple");
+		});
+		$("#green-c").click(function(){
+
+			$("#image-c").attr({"src":"bp-colors/classic/green.jpg"});
+			$("#colorname").text("Forest Green");
+		});
+		$("#light-green-c").click(function(){
+
+			$("#image-c").attr({"src":"bp-colors/classic/light-green.jpg"});
+			$("#colorname").text("Frost Green");
+		});
+		$("#yellow-c").click(function(){
+
+			$("#image-c").attr({"src":"bp-colors/classic/yellow.jpg"});
+			$("#colorname").text("Warm Yellow");
+		});
+		$("#grey-c").click(function(){
+
+			$("#image-c").attr({"src":"bp-colors/classic/grey.jpg"});
+			$("#colorname").text("Fog");
+		});
+		$("#red-green-c").click(function(){
+
+			$("#image-c").attr({"src":"bp-colors/classic/green-red.jpg"});
+			$("#colorname").text("Forest Green/Ox Red");
+		});
+
+
+
+/* for big backpacks */
+
+
+			
+	
+		$("#graphite-b").click(function(){
+
+			$("#image-b").attr({"src":"bp-colors/big/grapite.jpg"});
+			$("#colorname").text("Graphite");
+		});
+		
+		$("#red-b").click(function(){
+
+			$("#image-b").attr({"src":"bp-colors/big/red.jpg"});
+			$("#colorname").text("Ox Red");
+		});
+		$("#royal-blue-b").click(function(){
+
+			$("#image-b").attr({"src":"bp-colors/big/royal-blue.jpg"});
+			$("#colorname").text("Royal Blue");
+		});
+		
+		$("#black-b").click(function(){
+
+			$("#image-b").attr({"src":"bp-colors/big/black.jpg"});
+			$("#colorname").text("Black");
+		});
+		
+		$("#violet-b").click(function(){
+
+			$("#image-b").attr({"src":"bp-colors/big/violet.jpg"});
+			$("#colorname").text("Purple");
+		});
+		$("#green-b").click(function(){
+
+			$("#image-b").attr({"src":"bp-colors/big/green.jpg"});
+			$("#colorname").text("Forest Green");
+		});
+		$("#grey-b").click(function(){
+
+			$("#image-b").attr({"src":"bp-colors/big/grey.jpg"});
+			$("#colorname").text("Fog");
+		});
+		
+/* for no.2 backpacks */
+		
+		
+		$("#red-n").click(function(){
+
+			$("#image-n").attr({"src":"bp-colors/no_2/red.jpg"});
+			$("#colorname").text("Deep Red");
+		});
+		$("#royal-blue-n").click(function(){
+
+			$("#image-n").attr({"src":"bp-colors/no_2/blue.jpg"});
+			$("#colorname").text("Lake Blue");
+		});
+		
+		$("#sand").click(function(){
+
+			$("#image-n").attr({"src":"bp-colors/no_2/sand.jpg"});
+			$("#colorname").text("Sand");
+		});
+		
+		$("#black-n").click(function(){
+
+			$("#image-n").attr({"src":"bp-colors/no_2/black.jpg"});
+			$("#colorname").text("Black");
+		});
+		$("#navy").click(function(){
+
+			$("#image-n").attr({"src":"bp-colors/no_2/navy.jpg"});
+			$("#colorname").text("Navy");
+		});
+		
+});
