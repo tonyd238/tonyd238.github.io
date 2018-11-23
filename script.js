417d837ad328a76ae7d1a1d029a9002e0e6df01e
tonyd238 <42108517+tonyd238@users.noreply.github.com>
2018-11-23 15:21:01 +0200

    афваф

--- script.js
+++ script.js
@@ -1,45 +1,19 @@
 $(document).ready(function(){
 
-	$('.slider-1').slick({
-		slidesToShow:2,
-		autoplay:false,
-		arrows:true,
-		slidesToScroll:2,
-		prevArrow: '<div class="slick-arrow-l-1"></div>',
-		nextArrow: '<div class="slick-arrow-r-1"></div>'
-	});
-	$('.comment-slider').slick({
-		slidesToShow:1,
-		arrows:true,
-		prevArrow: '<div class="previous"></div>',
-		nextArrow:'<div class="next"></div>'
-	});
+	if ($(window).width() <= 1024 ) {
 
-	$('.go_to').click( function(){
-	var scroll_el = $(this).attr('href');
-        if ($(scroll_el).length != 0) {
-	    $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 1100);
-        }
-	    return false;
-    });
-
-  	$('body').append('<a href="#" id="go-top" title="Вверх"></a>');
-	$(function() {
-	 	$.fn.scrollToTop = function() {
-	  	$(this).hide().removeAttr("href");
-	  	if ($(window).scrollTop() >= "250") $(this).fadeIn("slow")
-	  	var scrollDiv = $(this);
-	  	$(window).scroll(function() {
-	   	if ($(window).scrollTop() <= "250") $(scrollDiv).fadeOut("slow")
-	   	else $(scrollDiv).fadeIn("slow")
-	  	});
-	  	$(this).click(function() {
-	   	$("html, body").animate({scrollTop: 0}, "slow")
-	  	})
-	 	}
+	$("#mobile-nav").click(function(){
+		$(".mobile-navigation").animate({opacity:1},500);
+		$(".mobile-navigation").css('display','block');
+		$(".mobile-navigation > *").css('display','block');
+		$(".contacts-block").css('display','flex');	
 	});
-
-		$(function() {
-		 $("#go-top").scrollToTop();
+	$(".close").click(function(){
+		$(".mobile-navigation").animate({opacity:0},500);
+		$(".mobile-navigation").css('display','none');
+		$(".mobile-navigation > *").css('display','none');
+		$(".contacts-block").css('display','none');	
+		$(".header").css('display','none');
 	});
+}
 });
